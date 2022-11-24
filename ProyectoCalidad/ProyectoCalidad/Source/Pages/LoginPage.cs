fc177231157328671d4a91817d4344dc8aa4d701
Isabel <isagaleano9@gmail.com>
2022-11-23 19:33:31 -0600

    Add prueba login

---
+++ ProyectoCalidad/ProyectoCalidad/Source/Pages/LoginPage.cs
@@ -0,0 +1,49 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace ProyectoCalidad.Source.Pages
+{
+    public class LoginPage
+    {
+        private IWebDriver driver;
+
+
+        [FindsBy(How = How.XPath, Using = "//*[@id='headerPrimary']/a[4]")]
+        private IWebElement linkSignIn;
+
+        [FindsBy(How = How.ClassName, Using = "isUGLz")]
+        private IList<IWebElement> elements;
+
+        [FindsBy(How = How.Id, Using = "username")]
+        private IWebElement email;
+
+        [FindsBy(How = How.Id, Using = "password")]
+        private IWebElement password;
+
+        [FindsBy(How = How.Id, Using = "login")]
+        private IWebElement btnLogin;
+        public LoginPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        public void login(string vemail, string vpassword)
+        {
+            linkSignIn.Click();
+            Thread.Sleep(2000);
+            IWebElement element = elements[0];
+            element.Click();
+            Thread.Sleep(4000);
+            email.SendKeys(vemail);
+            password.SendKeys(vpassword);
+            Thread.Sleep(4000);
+            btnLogin.Click();
+            Thread.Sleep(4000);
+        }
+    }
+}
---
+++ ProyectoCalidad/ProyectoCalidad/Tests/LoginTest.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using ProyectoCalidad.Source.Pages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProyectoCalidad.Tests
+{
+    public class LoginTest
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void initScript()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("--incognito");
+            driver = new ChromeDriver(options);
+
+            
+        }
+
+        [Test]
+        public void login()
+        {
+            LoginPage productPage = new LoginPage(driver);
+            driver.Navigate().GoToUrl("https://www.target.com/");
+            productPage.login("julianStartupsafe@gmail.com",  "Gatos123!");
+            //Assert.True(this.driver.Title.Contains("Cart : Target"));
+        }
+
+        [TearDown]
+        public void cleanup()
+        {
+            driver.Quit();
+        }
+    }
+}
