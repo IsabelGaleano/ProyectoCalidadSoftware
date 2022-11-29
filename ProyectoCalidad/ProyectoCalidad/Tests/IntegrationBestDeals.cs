ac5d20ad41583dbafb47959527815388f96fbda3
hbonilla96 <hbonillab@ucenfotec.ac.cr>
2022-11-28 21:11:38 -0600

    integration with best deals and deals of the day

---
+++ ProyectoCalidad/ProyectoCalidad/Tests/IntegrationBestDeals.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using ProyectoCalidad.Source.Pages;
+
+namespace ProyectoCalidad.Tests
+{
+
+    internal class IntegrationBestDeals
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void initScript()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("--disable-blink-features=AutomationControlled");
+            options.AddArgument("--incognito");
+            driver = new ChromeDriver(options);
+        }
+
+        [Test]
+        public void validateRedirect()
+        {
+            HomePage home = new HomePage(driver);
+            driver.Navigate().GoToUrl("https://www.bestbuy.com/?intl=nosplash");
+            home.goToTopDeals();
+            Assert.True(this.driver.Url.Equals("https://www.bestbuy.com/site/electronics/top-deals/pcmcat1563299784494.c?id=pcmcat1563299784494"));
+        }
+    }
+}
